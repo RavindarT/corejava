697cfc8dc2830b4bc52b65e511e719180cb0fcfd
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-04 13:58:13 -0400

    code committed

---
+++ src/main/java/com/core/ActualClass.java
@@ -0,0 +1,14 @@
+package com.core;
+
+import com.core.bean.SampleBean;
+
+public class ActualClass {
+	
+	public static void main(String ...strings ) {
+		
+		
+//		SampleBean s = new SampleBean(name : "Name", number: 3);
+		
+	}
+
+}
---
+++ src/main/java/com/core/bean/SampleBean.java
@@ -0,0 +1,19 @@
+package com.core.bean;
+
+public class SampleBean {
+	
+	String name;
+	
+	int number;
+
+	public SampleBean(String name, int number) {
+		super();
+		this.name = name;
+		this.number = number;
+	}
+	
+	public SampleBean() {
+		
+	}
+
+}
