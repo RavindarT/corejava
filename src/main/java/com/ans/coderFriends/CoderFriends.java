30f35ae1dd1b50d95e9b95b7c22689be62e15a9b
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-05 07:09:52 -0400

    comit

--- src/main/java/com/ans/coderFriends/CoderFriends.java
+++ src/main/java/com/ans/coderFriends/CoderFriends.java
@@ -6,31 +6,44 @@ public class CoderFriends {
 		  // Write your code here     
 		  int countE = 0;  
 		  int countB = 0;  
+		  
 		  char[] ericaCharArray = erica.toCharArray();  
+		  
 		  char[] bobCharArray = bob.toCharArray();    
-		  for(char c : ericaCharArray)          
-			  countE += difficulty(c);    
-		  for(char c : bobCharArray)  
+		  
+		  for(char c : ericaCharArray) {          
+			  countE += difficulty(c);
+		  }
+		  
+		  for(char c : bobCharArray) {  
 			  countB += difficulty(c);    
-		  if(countE > countB)        
-			  return "Erica";     
-		  else if(countE == countB)   
-			  return "Tie";      
+		  }
+		  
+		  if(countE > countB)  {      
+			  return "Erica"; 
+		  } else if(countE == countB) {
+			  return "Tie";
+		  }
+
 		  return "Bob";    
 	  }
 	  
 	  
 	  public static int difficulty(char c){   
 		  switch(c){          
+		  	
 		  	case 'e':      
 		  	case 'E':    
-		  		return 1;     
+		  		return 1; 
+		  		
 		  	case 'm':     
 		  	case 'M':      
-		  		return 3;       
+		  		return 3;  
+		  		
 		  	case 'h':       
 		  	case 'H':
 		  	   return 5; 
+		  	   
 		  	default: 
 		  		return 0;   
 		  }  
